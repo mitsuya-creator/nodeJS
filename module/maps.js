73ae4b5e7d3b3e9b4e2557f88568d77e04982494
@code <@code>
2023-05-20 07:20:34 +0700

    maps

--- module/maps.js
+++ module/maps.js
@@ -1,8 +1,9 @@
-const fruits = new Map([
-    ['apple', 400],
-    ['mango', 500],
-    ['orange', 100],
+const apple = { name: 'apple' };
+const orange = { name: 'orange' };
+const mango = { name: 'mango' };
 
-])
-
+const fruits = new Map();
+fruits.set(apple, 400);
+fruits.set(orange, 30);
+fruits.set(mango, 10);
 export default fruits;
