789dff8e56d1adbaccb632c3ecefb80d2e1979ce
projectx <@test.com>
2023-05-20 21:38:21 +0700

    Object.defineProperty

--- module/object.js
+++ module/object.js
@@ -2,7 +2,7 @@ function Mhs(firstName,lastName,lang){
   this.firstName = firstName;
   this.lastName = lastName;
   this.lang = lang;
-  this.fullName =  () => {
+  this.fullName =  function(){
     return `${this.firstName} ${this.lastName}`;
   }
 }
