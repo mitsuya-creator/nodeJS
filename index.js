41e3ea780a7f8ab26a4a2b0dac600fe5f60fe94c
projectx <@test.com>
2023-05-20 23:07:51 +0700

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import Mhs from './module/object.js';
+import { Mhs, person } from './module/function.js';
 
 const mitsuya = new Mhs('Mitsuya','Takashi','EN');
 const draken = new Mhs('Ryuzi','Draken','EN');
+
 console.log(mitsuya);
+console.log(person.fullName.call(mitsuya));
 console.log(draken);
-Object.defineProperty(draken, 'lang', {writable: true});
-draken['lang'] = 'FRA';
-console.log(draken);
+console.log(person.fullName.call(draken));
